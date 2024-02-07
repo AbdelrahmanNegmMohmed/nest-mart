bff02c9a3cd8d396c7812d87cc709fa63470b40a
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-02-07 17:13:24 +0200

    function drop dowen

--- src/components/SelectDrop/SelectDrop.jsx
+++ src/components/SelectDrop/SelectDrop.jsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SelectDrop.css"
 
 const SelectDrop = () => {
+    const[isOpenSelect,setisOpenSelect]=useState(false)
+    const openselect=()=>{
+        setisOpenSelect(!isOpenSelect)
+    }
+
+
+
   return (
     <div className="selectdrop cursor position-relative">
-        <span className='openselect'> All Categories</span>
-    <div className="SelectDrop">
-        <div className="searchfield">
-            <input type="text" />          
-        </div>
-        <ul className='searchresults'>
-                <li>Milks and Dairies</li>
-                <li>Wines & Drinks </li>
-                <li>Clothing & beauty</li>
-                <li>Fresh Seafood</li>
-                <li>Pet Foods & Toy</li>
-                <li>Fast Food</li>
-                <li>Baking material</li>
-                <li>Vegetables</li>
-                <li>Fresh Fruit</li>
-                <li>Bread And Juice</li>
-                <li>Milks and Dairies</li>
-                <li>Wines & Drinks</li>
-                <li>Clothing & beauty</li>
-                <li>Wines & Drinks</li>
-                <li>Fresh Seafood</li>
-            </ul>
+        <span className='openselect' onClick={openselect}> All Categories</span>
+        {isOpenSelect===true &&
+            <div className="SelectDrop">
+            <div className="searchfield">
+                <input type="text" />          
+            </div>
+            <ul className='searchresults'>
+                    <li onClick={()=>setisOpenSelect(false)}>Milks and Dairies</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Wines & Drinks </li>
+                    <li onClick={()=>setisOpenSelect(false)}>Clothing & beauty</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Fresh Seafood</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Pet Foods & Toy</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Fast Food</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Baking material</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Vegetables</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Fresh Fruit</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Bread And Juice</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Milks and Dairies</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Wines & Drinks</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Clothing & beauty</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Wines & Drinks</li>
+                    <li onClick={()=>setisOpenSelect(false)}>Fresh Seafood</li>
+                </ul>
+            
+         </div>
         
-     </div>
+        
+        }
+
   </div>
   
   )
