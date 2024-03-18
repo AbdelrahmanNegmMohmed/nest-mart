e5a4cc1fe9e964181b67d4cd3c0a9d74ea5baa4d
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-03-18 23:24:07 +0200

    Google Authentication with React Js and Firebase   Sign in with Google in React Js and Firebase ���

--- src/Pages/signIn/SignUp.jsx
+++ src/Pages/signIn/SignUp.jsx
@@ -65,14 +65,22 @@ const Signin = () => {
 
 
   const signInWithGoogle =() =>{
+    setShowLoader(true);
     signInWithPopup(auth, googleProvider)
   .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     // The signed-in user info.
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
+
+    setShowLoader(false);
+/*     setFormFields({
+      email:"",
+      password:"",
+    }); */
+    localStorage.setItem('isLogin',true);
+    context.signIn();
+    history('/ ')
     // ...
   }).catch((error) => {
     // Handle Errors here.
